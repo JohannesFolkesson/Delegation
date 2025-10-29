c4164b435bdc83924a3993f5ef0d772e1bf5387e
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-10-29 11:50:59 +0100

    delegation practice

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector('ul').addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+
+        console.log('Du klickade på:', e.target.textContent);
+    }
+
+})
+
+const ul = document.querySelector('ul');
+
+const btn = document.createElement('button');
+btn.textContent = 'Lägg till ny li';
+document.body.appendChild(btn);
+
+btn.addEventListener('click', () => {
+    const newLi = document.createElement('li')
+    newLi.textContent = 'Banan'
+    ul.appendChild(newLi);
+})
+
