8be6bfd7583ac1a4a53c2559635e24c4afc82f20
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-02 18:37:20 +0100

    practice 5

--- Practice-2/script.js
+++ Practice-2/script.js
@@ -1,6 +1,7 @@
 const filters = document.getElementById('filters');
 const list = document.getElementById('todo-list');
 
+
 filters.addEventListener('click', (event) => {
     if(!event.target.matches('button')) return;
     const filter = event.target.dataset.filter;
