642f8d75eacfcf0c6f1244a01571ac5f081d5e71
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-03 08:09:30 +0100

    practice 5

--- Practice-4/script.js
+++ Practice-4/script.js
@@ -3,7 +3,6 @@ function myList() {
 const input = document.querySelector('showList');
 const list = document.querySelector('ul')
 
-
 input.addEventListener('keydown', (event) => {
     if(event.key === 'Enter' && input.value.trim() !== "") {
         let li = document.createElement('li');
